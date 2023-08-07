71176c056b5ed4da547d753236e9e47041aaaa62
fuhan8613 <140045742+fuhan8613@users.noreply.github.com>
2023-08-07 14:43:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import overheads,cash_on_hand,profit_loss
+def main():
+    overheads.overheads()
+    cash_on_hand.cash_on_hand()
+    profit_loss.profit_loss()   
+main()
---
+++ overheads.py
@@ -0,0 +1,134 @@
+def overheads():     
+     from pathlib import Path
+     import csv
+
+     # Create a file to csv file
+     fp = Path.cwd()/"csv_reports/Overheads.csv"
+
+     # Read the csv file to append Overhead Category and Overhead Value from the csv
+     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+          reader = csv.reader(file)
+          next(reader) # Skip header
+
+     # Create an empty list to store Overhead Category and Overhead Value
+          Overhead_Records = []
+
+     # Append Overhead Category and Overhead Value into the Overhead_Records list
+          for row in reader:
+               Overhead_Records.append([row[1],row[3]])
+          print(Overhead_Records)
+          print()
+
+     # Append all Overhead Category to Overhead_Category list
+     Overhead_Category = []
+     for i in range(len(Overhead_Records)):
+          Overhead_Category.append(Overhead_Records[i][0])
+
+     # Append the different types of Overhead Category to OC list
+     OC = []
+     for i in Overhead_Category:
+          if i not in OC:
+               OC.append(i)
+     print(OC)
+     print()
+
+     # Create lists to store each type of Overhead Category values
+     Human_Resource_Expense = []
+     Marketing_Expense = []
+     Salary_Expense = []
+     Shipping_Expense = []
+     Depreciation_Expense = []
+     Rental_Expense = []
+     Maintenance_Expense = []
+     Interest_Expense = []
+     Overflow_Expense_Warehouse = []
+     Penalty_Expense = []
+     Overflow_Expense_Retail = []
+
+     # Create a list to store all the Overhead values
+     Total_Overhead_Expense = []
+
+     # Append Overhead Values into their respective Overhead Category lists
+     for i in range(len(Overhead_Records)):
+          Total_Overhead_Expense.append(float(Overhead_Records[i][1]))
+          if Overhead_Records[i][0] == "Human Resource Expense":
+               Human_Resource_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Marketing Expense":
+               Marketing_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Salary Expense":
+               Salary_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Shipping Expense":
+               Shipping_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Depreciation Expense":
+               Depreciation_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Rental Expense":
+               Rental_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Maintenance Expense":
+               Maintenance_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Interest Expense ":
+               Interest_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Overflow Expense - Warehouse":
+               Overflow_Expense_Warehouse.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Penalty Expense":
+               Penalty_Expense.append(float(Overhead_Records[i][1]))
+          elif Overhead_Records[i][0] == "Overflow Expense - Retail":
+               Overflow_Expense_Retail.append(float(Overhead_Records[i][1]))
+          
+     print(Human_Resource_Expense)
+     print()
+     print(Marketing_Expense)
+     print()
+     print(Salary_Expense)
+     print()
+     print(Shipping_Expense) 
+     print()
+     print(Depreciation_Expense)
+     print() 
+     print(Rental_Expense)
+     print()
+     print(Maintenance_Expense)
+     print()
+     print(Interest_Expense) 
+     print()
+     print(Overflow_Expense_Warehouse) 
+     print()
+     print(Penalty_Expense) 
+     print()
+     print(Overflow_Expense_Retail) 
+     print()
+
+     # Calculate the percentage for each Overhead Category values and append it to Overhead_Percentage list
+     Overhead_Percentage = []
+     Human_Resource_Expense_P = (sum(Human_Resource_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Human_Resource_Expense_P)
+     Total_Marketing_Expense_P = (sum(Marketing_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Marketing_Expense_P)
+     Total_Salary_Expense_P = (sum(Salary_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Salary_Expense_P)
+     Total_Shipping_Expense_P = (sum(Shipping_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Shipping_Expense_P)
+     Total_Depreciation_Expense_P = (sum(Depreciation_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Depreciation_Expense_P)
+     Total_Rental_Expense_P = (sum(Rental_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Rental_Expense_P)
+     Total_Maintenance_Expense_P = (sum(Maintenance_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Maintenance_Expense_P)
+     Total_Interest_Expense_P = (sum(Interest_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Interest_Expense_P)
+     Total_Overflow_Expense_Warehouse_P = (sum(Overflow_Expense_Warehouse) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Overflow_Expense_Warehouse_P)
+     Total_Penalty_Expense_P = (sum(Penalty_Expense) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Penalty_Expense_P)
+     Total_Overflow_Expense_Retail_P = (sum(Overflow_Expense_Retail) / sum(Total_Overhead_Expense)) * 100
+     Overhead_Percentage.append(Total_Overflow_Expense_Retail_P)
+     print(Overhead_Percentage)
+     print()
+     
+     # Find the posititon of the highest overhead in Overhead_Percentage list
+     Position = Overhead_Percentage.index(max(Overhead_Percentage))
+     print(f'[HIGHEST OVERHEAD] {(OC[Position]).upper()}:{max(Overhead_Percentage):.2f}%')
+
+     with open("summary_report.txt","w") as file:
+          file.write(f'[HIGHEST OVERHEAD] {(OC[Position]).upper()}:{max(Overhead_Percentage):.2f}%\n')
+     return(f'[HIGHEST OVERHEAD] {(OC[Position]).upper()}:{max(Overhead_Percentage):.2f}%\n')
+
