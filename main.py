ffa8810b9b438bc703de04369d2d791aadee9165
fuhan8613 <140045742+fuhan8613@users.noreply.github.com>
2023-08-09 23:46:25 +0800

    Update main.py
    documentation changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import overheads,cash_on_hand,profit_loss
 def main():
-    overheads.overheads()
-    cash_on_hand.cash_on_hand()
-    profit_loss.profit_loss()   
+    overheads.overheads() # Call overheads function in main function
+    cash_on_hand.cash_on_hand() # Call cash_on_hand function in main function
+    profit_loss.profit_loss() # Call profit_loss function in main function
 main()
