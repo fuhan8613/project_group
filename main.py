9f543b323002cde7e98cf3d5a01b53099a98a63d
fuhan8613 <140045742+fuhan8613@users.noreply.github.com>
2023-08-10 00:33:39 +0800

    Update main.py
    documentation changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# main.py imports each python file as modules
 import overheads,cash_on_hand,profit_loss
+# main.py essentially acts as the coordinating program to execute each python file
 def main():
-    overheads.overheads() # Call overheads function in main function
-    cash_on_hand.cash_on_hand() # Call cash_on_hand function in main function
-    profit_loss.profit_loss() # Call profit_loss function in main function
+    overheads.overheads() # Gives the Highest Overhead percentage
+    cash_on_hand.cash_on_hand() # Gives the Highest Cash Surplus and the Day or the Cash Deficit and the Day
+    profit_loss.profit_loss() # Gives the Highest Net Profit Surplus and the Day or the Profit Deficit and the Day
 main()
